067d3b24795fe0284b2dd90e20e71bb54b0bcdfb
Yurii77777 <yuretshome@gmail.com>
2021-11-16 19:12:01 +0200

    Add land plot inputs validation and handler inputs data. Implement calculate land plot square. TODO:// Inputs error messages

--- js/main.js
+++ js/main.js
@@ -7,68 +7,140 @@
 
 "use strict";
 
-const elementsNavTabs = document.getElementsByClassName('constructor__elements-nav');
-const elementsNavTabsValues = document.getElementsByClassName('constructor__elements-container');
-
 /**
- * Блок для роботи з вкладками елементів конструктора (показати чи приховати)
- * Handle constructor's Nav Tabs (show or hide)
- * Блок для работы с вкладками элементов конструктора (показать или скрыть)
+ * Блок для роботи з вкладками елементів конструктора (показати / приховати)
+ * Handle constructor's Nav Tabs (show / hide)
+ * Блок для работы с вкладками элементов конструктора (показать / скрыть)
  */
-elementsNavTabs[0].addEventListener('click', e => {
-    e.stopPropagation();
+const elementsNavTabs = document.getElementsByClassName(
+  "constructor__elements-nav"
+);
+const elementsNavTabsValues = document.getElementsByClassName(
+  "constructor__elements-container"
+);
 
-    let userClick = e.target;
-    const navTabs = Array.from(elementsNavTabs[0].children);
-    let selectedNavTabNumber = navTabs.indexOf(userClick);
-    const navTabsValues = Array.from(elementsNavTabsValues[0].children);
+elementsNavTabs[0].addEventListener("click", (e) => {
+  e.stopPropagation();
 
-    for (let i = 0; i < navTabs.length; i ++) {
-        navTabs[i].classList.remove('active');
-        navTabsValues[i].classList.remove('active');
+  let userClick = e.target;
+  const navTabs = Array.from(elementsNavTabs[0].children);
+  let selectedNavTabNumber = navTabs.indexOf(userClick);
+  const navTabsValues = Array.from(elementsNavTabsValues[0].children);
 
-        navTabs[selectedNavTabNumber].classList.add('active');
-        navTabsValues[selectedNavTabNumber].classList.add('active');
-    }
+  for (let i = 0; i < navTabs.length; i++) {
+    navTabs[i].classList.remove("active");
+    navTabsValues[i].classList.remove("active");
+
+    navTabs[selectedNavTabNumber].classList.add("active");
+    navTabsValues[selectedNavTabNumber].classList.add("active");
+  }
 });
 
 /**
- * Блок для роботи з полями ввода зміни розмірів ділянки землі
+ * Блок для роботи з полями ввода зміни розмірів ділянки землі (показати / приховати)
  */
 let isVisibleEditPlotSizes = false;
-const arrowsContainerNode = document.querySelectorAll('.field__land-plot-show-edit');
-const editPlotSizesNode = document.querySelectorAll('.field__land-plot-user-edit');
+const arrowsContainerNode = document.querySelectorAll(
+  ".field__land-plot-show-edit"
+);
+const editPlotSizesNode = document.querySelectorAll(
+  ".field__land-plot-user-edit"
+);
 
-//TODO: Validate and getting data from inputs
-const handleEditPlotInputs = () => {
-    let newWidth = null;
-    let newLength = null;
+arrowsContainerNode[0].addEventListener("click", (e) => {
+  e.stopPropagation();
 
-    let inputPlotWidth = document.querySelectorAll('.land-plot-user-edit__plot-width');
-    let inputPlotLength = document.querySelectorAll('.land-plot-user-edit__plot-length');
+  const arrowsContainer = Array.from(arrowsContainerNode[0].children);
+  let userClick = e.target;
 
-    return {newWidth, newLength};
+  if (!isVisibleEditPlotSizes) {
+    isVisibleEditPlotSizes = true;
+    arrowsContainer[0].classList.remove("active");
+    arrowsContainer[1].classList.add("active");
+    editPlotSizesNode[0].classList.add("active");
+  } else {
+    isVisibleEditPlotSizes = false;
+    arrowsContainer[1].classList.remove("active");
+    arrowsContainer[0].classList.add("active");
+    editPlotSizesNode[0].classList.remove("active");
+  }
+});
+
+/**
+ * Функція валідації данних інпутів зміни земельної ділянки
+ * тільки числа, максимум 4 знаки, не менше базових габаритів
+ */
+let isNewWidth = false;
+let isNewLength = false;
+const plotWidthInput = document.querySelectorAll(
+  ".land-plot-user-edit__plot-width"
+);
+const plotLengthInput = document.querySelectorAll(
+  ".land-plot-user-edit__plot-length"
+);
+let newPlotWidth = null;
+let newPlotLength = null;
+
+const validateEditPlotInputs = () => {
+  newPlotWidth = plotWidthInput[0].value.replace(/\D/g, "");
+  newPlotLength = plotLengthInput[0].value.replace(/\D/g, "");
+
+  if (newPlotWidth.length > 4 || newPlotWidth === "" || newPlotWidth < 140) {
+    plotWidthInput[0].classList.add("error");
+    isNewWidth = false;
+
+  } else {
+    plotWidthInput[0].classList.remove("error");
+    isNewWidth = true;
+  }
+
+  if (newPlotLength.length > 4 || newPlotLength === "" || newPlotLength < 220) {
+    plotLengthInput[0].classList.add("error");
+    isNewLength = false;
+  } else {
+    plotLengthInput[0].classList.remove("error");
+    isNewLength = true;
+  }
+};
+
+/**
+ * Функція застосування валідних даних в конструктор
+ */
+const landPlotWidth = document.querySelectorAll(".field__land-plot-width");
+const landPlotLength = document.querySelectorAll(".field__land-plot-length");
+const calculatorLandPlotNode = document.querySelectorAll(
+    ".calculator__data-value.land-plot"
+  );
+const landPlotSquareNode = document.querySelectorAll('.calculator__data-value.land-pot-square');
+const plotInputErrorMessage = document.querySelectorAll('.field__error');
+
+const handlePlotInputsData = () => {
+  if (isNewWidth && isNewLength) {
+    landPlotWidth[0].innerText = newPlotWidth;
+    landPlotLength[0].innerText = newPlotLength;
+    calculatorLandPlotNode[0].innerText = `${newPlotWidth} x ${newPlotLength}`;
+    landPlotSquareNode[0].innerText = (newPlotWidth * newPlotLength) / 10000;
+
+    plotWidthInput[0].value = "";
+    plotLengthInput[0].value = "";
+    editPlotSizesNode[0].classList.remove("active");
+  } else {
+    plotInputErrorMessage[0].classList.add("active");
+
+    setTimeout(() => {
+        plotInputErrorMessage[0].classList.remove("active");
+    }, 3000)
+  }
+};
+
+/**
+ * Блок калькулятора
+ */
+
+if (!isNewWidth && !isNewLength) {
+    calculatorLandPlotNode[0].innerText = `${landPlotWidth[0].innerText} x ${landPlotLength[0].innerText}`;
+    landPlotSquareNode[0].innerText = (+landPlotWidth[0].innerText * +landPlotLength[0].innerText) / 10000;
 }
-
-arrowsContainerNode[0].addEventListener('click', e => {
-    e.stopPropagation();
-
-    const arrowsContainer = Array.from(arrowsContainerNode[0].children);
-    let userClick = e.target;
-    
-    if (!isVisibleEditPlotSizes) {
-        isVisibleEditPlotSizes = true;
-        arrowsContainer[0].classList.remove('active');
-        arrowsContainer[1].classList.add('active');
-        editPlotSizesNode[0].classList.add('active');
-
-    } else {
-        isVisibleEditPlotSizes = false;
-        arrowsContainer[1].classList.remove('active');
-        arrowsContainer[0].classList.add('active');
-        editPlotSizesNode[0].classList.remove('active');
-    }
-})
 
 /* Ниже черновички "позже к ним обратимся" */
 
@@ -127,7 +199,7 @@ arrowsContainerNode[0].addEventListener('click', e => {
 //   var file = e.target.files[0];
 //   var reader = new FileReader();
 //   reader.onload = function (f) {
-//     var data = f.target.result;                    
+//     var data = f.target.result;
 //     fabric.Image.fromURL(data, function (img) {
 //       var oImg = img.set({left: 0, top: 0, angle: 00,width:100, height:100}).scale(0.9);
 //       canvas.add(oImg).renderAll();
