de83516179a5aacbb09eb03bf5b7cf36e9130cee
Yurii77777 <yuretshome@gmail.com>
2021-11-29 16:05:43 +0200

    Add Stand elements. Testing draggable elements, such as stands, monuments etc. In process...

--- data/prices.js
+++ data/prices.js
@@ -111,5 +111,117 @@ const prices = [
                 "siteNameUa": "Плитка Тротуарна"
             },
         ]
-    }
+    },
+    {
+        "stand": [
+            {
+                "id": 0,
+                "titleUa": "Тумба 50х20х15",
+                "price": 1000,
+                "length": 50,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 50х20х15"
+            },
+            {
+                "id": 1,
+                "titleUa": "Тумба 50х20х20",
+                "price": 1100,
+                "length": 50,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 50х20х20"
+            },
+            {
+                "id": 2,
+                "titleUa": "Тумба 60х20х15",
+                "price": 1100,
+                "length": 60,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 60х20х15"
+            },
+            {
+                "id": 3,
+                "titleUa": "Тумба 60х20х20",
+                "price": 1400,
+                "length": 60,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 60х20х20"
+            },
+            {
+                "id": 4,
+                "titleUa": "Тумба 60х20х25",
+                "price": 1600,
+                "length": 60,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 60х20х25"
+            },
+            {
+                "id": 5,
+                "titleUa": "Тумба 70х20х20",
+                "price": 1900,
+                "length": 70,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 70х20х20"
+            },
+            {
+                "id": 6,
+                "titleUa": "Тумба 70х20х25",
+                "price": 2200,
+                "length": 70,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 70х20х25"
+            },
+            {
+                "id": 7,
+                "titleUa": "Тумба 70х20х30",
+                "price": 2500,
+                "length": 70,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 70х20х30"
+            },
+            {
+                "id": 8,
+                "titleUa": "Тумба 80х20х20",
+                "price": 2300,
+                "length": 80,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 80х20х20"
+            },
+            {
+                "id": 9,
+                "titleUa": "Тумба 80х20х25",
+                "price": 2500,
+                "length": 80,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 80х20х25"
+            },
+            {
+                "id": 10,
+                "titleUa": "Тумба 90х20х20",
+                "price": 2800,
+                "length": 90,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 90х20х20"
+            },
+            {
+                "id": 11,
+                "titleUa": "Тумба 1000х20х20",
+                "price": 3000,
+                "length": 100,
+                "tabId": 1,
+                "imgUrl": "/mobile/stand/stand_granite_black.png",
+                "siteNameUa": "Тумба 1000х20х20"
+            },
+        ]
+    },
 ];
--- js/main.js
+++ js/main.js
@@ -231,6 +231,50 @@ const handleLandPlotSizes = () => {
 };
 
 /**
+ * Фугкція для пересування елементів - Тумби
+ */
+dragElement(document.getElementById("stand-container"));
+
+function dragElement(elmnt) {
+    let pos1 = 0;
+    let pos2 = 0;
+    let pos3 = 0;
+    let pos4 = 0;
+
+    if (document.getElementById(elmnt.id)) {
+        document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+/**
  * Локалізація вузлів сторінки
  * Localization of page's nodes
  * Локализация узлов страницы
@@ -701,7 +745,9 @@ elementsValuesSocleNode[0].addEventListener("click", (e) => {
         );
 
         if (activeTileElement || activeTileElement === 0) {
-            elementsBeautification[activeTileElement].classList.remove("active");
+            elementsBeautification[activeTileElement].classList.remove(
+                "active"
+            );
             tileGraniteBlackImgOnConstructor[0].classList.remove("active");
             tileGraniteGrayImgOnConstructor[0].classList.remove("active");
             tileGres1ImgOnConstructor[0].classList.remove("active");
@@ -1004,49 +1050,6 @@ const calculate = () => {
 calculate();
 
 /* Ниже черновички "позже к ним обратимся" */
-
-// dragElement(document.getElementById("mydiv"));
-
-// function dragElement(elmnt) {
-//   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-//   if (document.getElementById(elmnt.id + "header")) {
-//     // if present, the header is where you move the DIV from:
-//     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-//   } else {
-//     // otherwise, move the DIV from anywhere inside the DIV:
-//     elmnt.onmousedown = dragMouseDown;
-//   }
-
-//   function dragMouseDown(e) {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // get the mouse cursor position at startup:
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     document.onmouseup = closeDragElement;
-//     // call a function whenever the cursor moves:
-//     document.onmousemove = elementDrag;
-//   }
-
-//   function elementDrag(e) {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // calculate the new cursor position:
-//     pos1 = pos3 - e.clientX;
-//     pos2 = pos4 - e.clientY;
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     // set the element's new position:
-//     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-//     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-//   }
-
-//   function closeDragElement() {
-//     // stop moving when mouse button is released:
-//     document.onmouseup = null;
-//     document.onmousemove = null;
-//   }
-// }
 
 // var canvas = new fabric.Canvas('canvas');
 // document.getElementById('file').addEventListener("change", function (e) {
